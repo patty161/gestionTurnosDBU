828295b7dd6a327cca06545b60e28c731f19447a
katerinbenavides <katerinbenavides@localhost>
2020-02-03 10:26:10 -0500

    cambios

--- gestionturnosEJB/ejbModule/gestionturnos/model/manager/ManagerTurnos.java
+++ gestionturnosEJB/ejbModule/gestionturnos/model/manager/ManagerTurnos.java
@@ -1,15 +1,21 @@
 package gestionturnos.model.manager;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import gestionturnos.model.entities.Area;
+import gestionturnos.model.entities.Asignacion;
+import gestionturnos.model.entities.Estado;
 import gestionturnos.model.entities.Turno;
+import gestionturnos.model.entities.Usuario;
 
 /**
  * Session Bean implementation class ManagerArea
@@ -17,13 +23,15 @@ import gestionturnos.model.entities.Turno;
 @Stateless
 @LocalBean
 public class ManagerTurnos {
+	private Date fecha1 = new Date();
+	Timestamp fecha = new Timestamp(fecha1.getTime());
+	private int idArea, idAsignacion, idUsuario;
 
 	@PersistenceContext
 	private EntityManager em;
 	private String idasi;
-	
-	
-    public String getIdasi() {
+
+	public String getIdasi() {
 		return idasi;
 	}
 
@@ -32,18 +40,88 @@ public class ManagerTurnos {
 	}
 
 	public ManagerTurnos() {
-    	
-    }
-    
-    public List<Turno> findAllDoctor(){
-    	String consulta = "SELECT a FROM Turno a order by id_turnos";
-    	Query q= em.createQuery(consulta, Turno.class);
-    	return q.getResultList();
-    }
-    public List<Turno> findfinDoctor(){
-    	String consulta = "SELECT a FROM Turno a where id_asignacion="+idasi;
-    	Query q= em.createQuery(consulta, Turno.class);
-    	return q.getResultList();
-    }
+
+	}
+
+	public Turno crearTurnoTmp() {
+		Turno turnoTmp = new Turno();
+		return turnoTmp;
+	}
+	public List<Area> findAllAreas() {
+		String consulta = "SELECT a FROM Area a";
+		Query q = em.createQuery(consulta, Area.class);
+		return q.getResultList();
+	}
+	public List<Turno> findAllDoctor() {
+		String consulta = "SELECT a FROM Turno a order by id_turnos";
+		Query q = em.createQuery(consulta, Turno.class);
+		return q.getResultList();
+	}
+
+	public List<Turno> findfinDoctor() {
+		String consulta = "SELECT t FROM Turno t where id_asignacion=" + idasi;
+		Query q = em.createQuery(consulta, Turno.class);
+		return q.getResultList();
+	}
+
+	public List<Turno> findAllTurnos() {
+		String consulta = "SELECT t FROM Turno t";
+		Query q = em.createQuery(consulta, Turno.class);
+		return q.getResultList();
+	}
+
+	public Area findAreaById(int idArea) {
+		Area area = em.find(Area.class, idArea);
+		return area;
+	}
+
+	public Estado findEstadoById(int idEstado) {
+		Estado estado = em.find(Estado.class, idEstado);
+		return estado;
+	}
+
+	public Turno findTurnoById(int idTurno) {
+		return em.find(Turno.class, idTurno);
+	}
+
+	public Usuario findUsuarioById(int idUsuario) {
+		Usuario usuario = em.find(Usuario.class, idUsuario);
+		return usuario;
+	}
+
+	public Asignacion findAsignacionById(int idAsignacion) {
+		Asignacion usuario = em.find(Asignacion.class, idAsignacion);
+		return usuario;
+	}
+
+	public void eliminarTurno(int IdTurno) {
+		Turno turno = findTurnoById(IdTurno);
+		if (turno != null)
+			em.remove(turno);
+	}
+
+	public void insertarTurno(int idUsuario, int idArea, int idAsignacion, int idEstado)
+			throws Exception {
+				
+		Turno turno = new Turno();
+		Turno turnoTemp = new Turno();
+
+		turno.setFecha(fecha);
+		Area a=em.find(Area.class, idArea);
+		turno.setEspArea(a);
+		
+		Asignacion asig= em.find(Asignacion.class, idAsignacion);
+		turno.setSegAsignacion(asig);
+		turno.setFecha(fecha);
+		em.persist(turnoTemp);
+		em.persist(turno);
+	}
+
+	public void insertarTurno(Turno turno) throws Exception {
+		
+		turno.setFecha(fecha);
+			em.persist(turno);
+	}
+
 
 }
--- gestionturnosWeb/src/gestionturnos/controller/BeanTurnos.java
+++ gestionturnosWeb/src/gestionturnos/controller/BeanTurnos.java
@@ -1,14 +1,22 @@
 package gestionturnos.controller;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import gestionturnos.model.entities.Area;
+import gestionturnos.model.entities.Asignacion;
 import gestionturnos.model.entities.Turno;
+import gestionturnos.model.entities.Usuario;
 import gestionturnos.model.manager.ManagerTurnos;
 
 @Named
@@ -18,13 +26,29 @@ public class BeanTurnos implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EJB
-	private ManagerTurnos managerdoctor;
+	private ManagerTurnos managerTurnos;
 	private List<Turno> listaTurnos;
 	private List<Turno> listaTurnosXdoc;
+	private List<Turno> UltimoTurno;
 	private Turno turno;
+	private Turno turnoTmp;
+	private boolean turnoTmpGuardado;
 	
-	
+	@Inject
+	private BeanLogin beanLogin;
 
+	@PersistenceContext
+	private EntityManager em;
+	
+	private Integer idUsuario;
+	private Integer idArea;
+	private Integer idAsignacion;
+	private Integer idEstado;
+
+	private Date fecha1 = new Date();
+	Timestamp fecha = new Timestamp(fecha1.getTime());
+
+	
 	public List<Turno> getListaTurnosXdoc() {
 		return listaTurnosXdoc;
 	}
@@ -33,11 +57,50 @@ public class BeanTurnos implements Serializable {
 		this.listaTurnosXdoc = listaTurnosXdoc;
 	}
 
+	public BeanTurnos() {
+
+	}
+
 	@PostConstruct
 	public void inicializar() {
-		listaTurnos= managerdoctor.findAllDoctor();
-		listaTurnosXdoc= managerdoctor.findfinDoctor();
-		turno=new Turno();
+		listaTurnos = managerTurnos.findAllDoctor();
+		listaTurnosXdoc = managerTurnos.findfinDoctor();
+		turnoTmp = new Turno();
+		turno = new Turno();
+	}
+
+	public String actionListenerInsertarTurno() {
+		Turno t= new Turno();
+//		Area a=em.find(Area.class, idArea);
+//		t.setEspArea(a);
+//		
+//		Asignacion asig= em.find(Asignacion.class, idAsignacion);
+//		t.setSegAsignacion(asig);
+		
+		//Usuario us= em.find(Usuario.class, idUsuario);
+		//t.setUsuario(us);
+		
+		t.setFecha(fecha);
+		
+			try {
+			managerTurnos.insertarTurno(t);	
+			listaTurnos = managerTurnos.findAllTurnos();
+			turno = new Turno();
+			JSFUtil.createMensajeInfo("Turno Generado");
+		} catch (Exception e) {
+			JSFUtil.createMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+		return "detalle.xhtml";
+	}
+
+	public void crearNuevoTurno() {
+		turnoTmp = managerTurnos.crearTurnoTmp();
+		idUsuario = null;
+		idAsignacion = null;
+		idArea = null;
+		idEstado = null;
+		setTurnoTmpGuardado(false);
 	}
 
 	public List<Turno> getListaTurnos() {
@@ -56,5 +119,63 @@ public class BeanTurnos implements Serializable {
 		this.turno = turno;
 	}
 
-	
+	public Turno getTurnoTmp() {
+		return turnoTmp;
+	}
+
+	public void setTurnoTmp(Turno turnoTmp) {
+		this.turnoTmp = turnoTmp;
+	}
+
+	public boolean isTurnoTmpGuardado() {
+		return turnoTmpGuardado;
+	}
+
+	public void setTurnoTmpGuardado(boolean turnoTmpGuardado) {
+		this.turnoTmpGuardado = turnoTmpGuardado;
+	}
+
+	public BeanLogin getBeanLogin() {
+		return beanLogin;
+	}
+
+	public void setBeanLogin(BeanLogin beanLogin) {
+		this.beanLogin = beanLogin;
+	}
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public Integer getIdArea() {
+		return idArea;
+	}
+
+	public void setIdArea(Integer idArea) {
+		this.idArea = idArea;
+	}
+
+	public Integer getIdAsignacion() {
+		return idAsignacion;
+	}
+
+	public void setIdAsignacion(Integer idAsignacion) {
+		this.idAsignacion = idAsignacion;
+	}
+
+	public Integer getIdEstado() {
+		return idEstado;
+	}
+
+	public List<Turno> getUltimoTurno() {
+		return UltimoTurno;
+	}
+
+	public void setUltimoTurno(List<Turno> ultimoTurno) {
+		UltimoTurno = ultimoTurno;
+	}
 }
